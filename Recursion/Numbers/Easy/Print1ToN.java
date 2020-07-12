d455e5b0c811de366906782ab0d59af7e96b0f29
Niraj Kumar <nirajkr.sm@gmail.com>
2020-07-12 22:32:04 +0530

    print 1 to N

--- Recursion/Numbers/Easy/Print1ToN.java
+++ Recursion/Numbers/Easy/Print1ToN.java
@@ -1,1 +1,26 @@
+/**
+  print 1 to N
 
+  time complexity: O(n)
+  space complexity: O(n)
+*/
+
+class Print1ToN {
+   public static void print1ToN(int n) {
+     // base case
+     if (n == 0) {
+       return;
+     }
+     else
+     { 
+     print1ToN(n - 1);
+     }
+     
+     System.out.println(n);
+     }
+ 
+    public static void main(String[] args) {
+      print1ToN(6);
+   }
+} 
+       
